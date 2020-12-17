2ebd74a930cccc339d873b38014e91ed100c5067
Ricardo Madeira <fc49434@alunos.fc.ul.pt>
2020-12-17 03:20:52 +0000

    Movimentacao e limite mapa

---
+++ Projeto DDJ/Assets/Scripts/EdgeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeScript : MonoBehaviour
+{
+    void Start(){
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+     Debug.Log(other.collider.name);
+         if (other.collider.name == "Player") {
+             other.collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.collider.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+         }
+     }
+
+
+}
--- Projeto DDJ/Assets/Scripts/PlayerMovement.cs
+++ Projeto DDJ/Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,32 @@ public class PlayerMovement : MonoBehaviour
 {
 
 
-
+public float speed = 5f;
+private Rigidbody2D r;
+ 
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        r = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+      void FixedUpdate()
     {
-        
+        float moveX = 0f;
+        float moveY = 0f;
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            moveY = +1f;
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            moveY = -1f;
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            moveX = -1f;
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+         moveX = +1f;
+
+         Vector3 moveDir = new Vector3(moveX,moveY).normalized;
+         transform.position += moveDir *speed * Time.deltaTime;
+     
     }
+
 }
