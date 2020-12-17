db1b12cef7dafe31ec61a7a736269b856d6329e9
Ricardo Madeira <fc49434@alunos.fc.ul.pt>
2020-12-17 17:28:08 +0000

    Camera following e andar esq ta fdd

---
+++ Projeto DDJ/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    void Update () 
+    {
+        transform.position = new Vector3 (player.position.x, player.position.y+2, player.position.z-3); // Camera follows the player but 6 to the right
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
--- Projeto DDJ/Assets/Scripts/PlayerMovement.cs
+++ Projeto DDJ/Assets/Scripts/PlayerMovement.cs
@@ -8,26 +8,34 @@ public class PlayerMovement : MonoBehaviour
 
 public float speed = 5f;
 private Rigidbody2D r;
+private Animator animator;
  
  
     // Start is called before the first frame update
     void Start()
     {
         r = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
       void FixedUpdate()
     {
         float moveX = 0f;
         float moveY = 0f;
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
             moveY = +1f;
-        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            animator.SetFloat("DireccaoY",1);
+        }if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
             moveY = -1f;
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            animator.SetFloat("DireccaoY",-1);
+        }if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
             moveX = -1f;
-        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-         moveX = +1f;
+            animator.SetFloat("DireccaoX",-1);
+        }if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            animator.SetFloat("DireccaoX",1);
+             moveX = +1f;
+        }
+        
 
          Vector3 moveDir = new Vector3(moveX,moveY).normalized;
          transform.position += moveDir *speed * Time.deltaTime;
