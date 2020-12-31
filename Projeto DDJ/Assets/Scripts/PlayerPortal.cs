6b2558a134024d0a064d013745fdd4db48043631
NegativeDigit <75028441+NegativeDigit@users.noreply.github.com>
2020-12-31 13:09:22 +0000

    teste

---
+++ Projeto DDJ/Assets/Scripts/PlayerPortal.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPortal : MonoBehaviour
+{
+
+    public Transform crosshair;
+    public SpriteRenderer crosshairSprite;
+    public GameObject objectToBeCreated;
+    private float distance; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = GameObject.Find("Player") .GetComponent<PlayerHook>().ropeMaxCastDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        var facingDirection = worldMousePosition - transform.position;
+        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        if (aimAngle < 0f)
+        {
+            aimAngle = Mathf.PI * 2 + aimAngle;
+        }
+        SetCrosshairPosition(aimAngle);
+         if (Input.GetKeyDown(KeyCode.Space))
+        {
+             CreateObject();
+             //crosshairSprite.enabled = false;
+         }
+    }
+    
+
+    public void CreateObject(){
+       Vector3 position    = new Vector3(crosshair.transform.position.x,crosshair.transform.position.y, crosshair.transform.position.z);
+        Quaternion rotation = new Quaternion(0,0,0,0);
+        GameObject obj      = Instantiate(objectToBeCreated, position, rotation) as GameObject;
+        obj.layer = 12;
+        obj.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        obj.transform.localScale = new Vector3(transform.localScale.x/2,transform.localScale.y/3, 0);
+       
+    }
+
+     /// <summary>
+    /// Move the aiming crosshair based on aim angle
+    /// </summary>
+    /// <param name="aimAngle">The mouse aiming angle</param>
+    private void SetCrosshairPosition(float aimAngle)
+    {
+        if (!crosshairSprite.enabled)
+        {
+            crosshairSprite.enabled = true;
+        }
+      
+        var x = transform.position.x + distance * Mathf.Cos(aimAngle);
+        var y = transform.position.y + distance * Mathf.Sin(aimAngle);
+        var crossHairPosition = new Vector3(x, y, 0);
+        Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        var mousePosition = new Vector3( worldPosition.x, worldPosition.y, 0);
+        var distMouse = Vector3.Distance(mousePosition,transform.position);
+        if(distMouse < distance)
+                crosshair.transform.position = mousePosition;
+                else
+                {
+                    crosshair.transform.position = crossHairPosition;
+                }
+        
+    }
+}
