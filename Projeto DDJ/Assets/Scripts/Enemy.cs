dac73ab8d4032abb40ab6b2dd892c4cbe5a0531e
André Cruz <andre.cruz@caixamagica.pt>
2021-01-27 16:45:30 +0000

    snowman

---
+++ Projeto DDJ/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name.Equals("Player")) {
+            gameManager.FailedLevel();
+        }
+
+    }
+}
