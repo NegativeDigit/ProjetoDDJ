069d2999b17e1fe5373bcb816cac53a08e75b9e1
NegativeDigit <75028441+NegativeDigit@users.noreply.github.com>
2021-02-05 11:50:23 +0000

    não me lembro lol

---
+++ Projeto DDJ/Assets/Scripts/ChooseLevelScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseLevelScript : MonoBehaviour
+{
+    public void SkyLevel()
+    {
+        SceneManager.LoadScene("Air_Level");
+    }
+}
